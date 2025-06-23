ed889cc19477a882ac510ee33a3aceeafd698bc7
HenrikFalla <henrik.falla@gmail.com>
2025-06-23 13:28:25 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'fhi-case-2025-06-23';
+}
